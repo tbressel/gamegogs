270e3decb9310a15bd636b9fb87f4ae3f36e5972
tbressel <tbressel.dev@gmail.com>
2023-08-31 04:10:02 +0200

    Export build fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+}
+module.exports = nextConfig;
+
+
 const path = require('path');
+
 module.exports = {
     sassOptions: {
         includePaths: [
             path.join(__dirname,'styles')
         ]
-    }
+    },
 };
